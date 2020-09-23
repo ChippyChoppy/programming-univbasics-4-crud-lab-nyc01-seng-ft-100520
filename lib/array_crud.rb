10d61f668ad8df5529c9f7c4d0a43ed3c56860e0
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-23 19:24:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   dinosaurs = ["Velociraptor", "Stegosaurus", "Pteradactyl", "Ankylosaur"]
-  dinosaurs.unshift("Triceratops")
+  dinosaurs.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)

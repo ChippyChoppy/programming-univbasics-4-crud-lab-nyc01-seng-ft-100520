2ef00a4c8e5d1fe31562d29df575f58c8e09d306
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-23 19:25:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  dinosaurs = ["Velociraptor", "Stegosaurus", "Pteradactyl", "Ankylosaur, arrays!"]
+  dinosaurs = ["Velociraptor", "Stegosaurus", "Pteradactyl", "Ankylosaur", "arrays!"]
   dinosaurs.pop
 end
 

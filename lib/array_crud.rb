cad9adda5ef8891e91528804256f2fb0ef2c2dac
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-23 19:29:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  dinosaurs = ["wow", "Velociraptor", "Stegosaurus", "Pteradactyl", "Ankylosaur"]
+  dinosaurs[0]
 end
 
 def retrieve_last_element_from_array(array)

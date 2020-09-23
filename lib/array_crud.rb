e19fd3d22da40a9eed9023434fc0bd14901bfbba
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-23 19:23:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,19 +7,23 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  dinosaurs = ["Velociraptor", "Stegosaurus", "Pteradactyl", "Ankylosaur"]
+  dinosaurs << "T-Rex"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  dinosaurs = ["Velociraptor", "Stegosaurus", "Pteradactyl", "Ankylosaur"]
+  dinosars.unshift("Triceratops")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  dinosaurs = ["Velociraptor", "Stegosaurus", "Pteradactyl", "Ankylosaur"]
+  dinosaurs.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  dinosaurs = ["Velociraptor", "Stegosaurus", "Pteradactyl", "Ankylosaur"]
+  dinosaurs.shift
 end
 
 def retrieve_element_from_index(array, index_number)
